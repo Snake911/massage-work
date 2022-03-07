9ad504356da02cd777d1e97637c78db02c453bb6
Volkov Danil <snake1234567890-1@yandex.ru>
2022-03-07 17:51:04 +0300

    feat: add sections contacts, request and requirements

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const mobileMenu = document.getElementById('menu_mobile');
 
     burger.addEventListener('click', () => {
+        const body = document.querySelector('body');
         burger.classList.toggle('active');
         mobileMenu.classList.toggle('active');
+        if(burger.classList.contains('active')) {
+            body.style.overflow = 'hidden';
+        } else {
+            body.style.overflow = '';
+        }
     });
 
     $('#about_slider').owlCarousel({
